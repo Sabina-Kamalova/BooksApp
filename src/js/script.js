ecf6fbdc34cdbc9f3690b50bd37990ed4b0f8a1d
Sabina Kamalova <skamalova09@gmail.com>
2023-02-09 22:33:11 +0100

    Add/remove favourites

--- src/js/script.js
+++ src/js/script.js
@@ -8,17 +8,17 @@
       listBooks: '.books-list'
     },
     book: {
-      image: '.books-list .book__image',
+      image: '.book__image',
     },
   };
   const templates = {
-    booksList: Handlebars.compile(document.querySelector(select.templateOf.books).innerHTML),
+    books: Handlebars.compile(document.querySelector(select.templateOf.books).innerHTML),
   };
 
   const render = function (){
     for (const book of dataSource.books){
     /* generate HTML based on template */
-      const generatedHTML = templates.booksList(book);
+      const generatedHTML = templates.books(book);
       /* create element using utils.createElementFromHTML */
       const elementDOM = utils.createDOMFromHTML(generatedHTML);
       /* find menu container */
@@ -27,5 +27,33 @@
       booksContainer.appendChild(elementDOM);
     }
   };
+  
+
+  
+  const favoriteBooks = [];
+
+  const initActions = function(){
+    const books = document.querySelectorAll(select.book.image);
+    for (let book of books){
+      book.addEventListener('dblclick', function(event){
+        event.preventDefault;
+        const bookId = book.getAttribute('data-id');
+        if(favoriteBooks.includes(bookId)){
+          book.classList.remove('favorite');
+          favoriteBooks.splice(favoriteBooks.indexOf(bookId), 1);
+        }
+        else {
+          book.classList.add('favorite');
+          favoriteBooks.push(bookId);
+        }
+        console.log('book', book);
+        console.log('favoriteBooks', favoriteBooks);
+      });
+    }
+  
+  };
   render();
+  initActions();
+
+
 }
